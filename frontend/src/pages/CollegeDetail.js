458aa34e999413b8366499adcf14d57879bdbf39
VEMURU-VENISHA <2300032694cser@gmail.com>
2026-04-29 11:27:22 +0530

    Update CollegeDetail.js

--- frontend/src/pages/CollegeDetail.js
+++ frontend/src/pages/CollegeDetail.js
@@ -1,4 +1,4 @@
-=import { useParams, useNavigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
 import API from "../api";   // ✅ ADD THIS
 
